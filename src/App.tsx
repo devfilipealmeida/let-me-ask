33f658c91ad676358dc1c61233fb180feb5a6876
filipedower <filipesilva504@gmail.com>
2021-11-12 17:35:59 -0400

    :passport_control: google autentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { NewRoom } from './pages/NewRoom'
-import { Home } from './pages/Home'
+import {BrowserRouter, Route} from 'react-router-dom'
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
 
 function App() {
   return (
-    <NewRoom />
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component= {NewRoom} />
+
+    </BrowserRouter>
   );
 }
 
